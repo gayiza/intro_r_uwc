375f9b86b3dc48a60dac43cdbdd01304a4550730
gayiza <3650799@myuwc.ac.za>
2019-02-01 15:13:20 +0200

    Day4_tidier & tidiest

---
+++ Day 4 _Tidier.R
@@ -0,0 +1,83 @@
+#Day 4
+#Chapter 11
+#Tidy data 2.0
+#1Feb 2019
+
+#load libraries
+library(tidyverse)
+library(lubridate)
+
+
+# Load the data from a .RData file
+load("data/SACTNmonthly_v4.0.RData")
+
+# Copy the data as a dataframe with a shorter name
+SACTN <- SACTNmonthly_v4.0
+
+# Remove the original
+rm(SACTNmonthly_v4.0)
+
+
+SACTN %>% 
+  filter(site == "Amanzimtoti") #using the equal to == to view data for only that site
+
+#using Logical operators
+SACTN %>% 
+  filter(site == "Pollock Beach", month(date) == 12 | month(date) == 1)   #we selecting the 12 month withing the date column or the 1st month
+
+
+#arrange data in depth and temp
+SACTN %>% 
+  arrange(depth, temp)     #arrange, arranges selected columns to the lowest to the highest value
+
+#arreange temp data descending
+SACTN %>% 
+  arrange(desc(temp))
+
+#show site Humewood and data for the year 1990
+SACTN %>% 
+  filter(site == "Humewood", year(date) == 1990)
+
+
+humewood_90s <- SACTN %>% 
+  filter(site == "Humewood", year(date) %in% seq(1990, 1999, 1))
+
+
+SACTN %>% 
+  filter(site == "Port Nolloth", # First give the site to filter
+         src == "DEA", # Then specify the source
+         temp <= 11 | # Temperatures at or below 11°C OR
+           is.na(temp) # Include missing values
+         
+ 
+         #chnically better or faster. So it is up to the user to find a favourite technique.
+         
+ # Select columns individually by name
+try1 <-  SACTN %>% 
+    select(site, src, date, temp)
+         
+# Select all columns between site and temp like a sequence
+try2 <-  SACTN %>% 
+  select(site:temp)                    #select data from site to temp
+         
+ # Select all columns except those stated individually
+ try3 <- SACTN %>% 
+  select(-date, -depth)
+         
+# Select all columns except those within a given sequence
+# Note that the '-' goes outside of a new set of brackets
+# that are wrapped around the sequence of columns to remove
+ try4 <- SACTN %>% 
+    select(-(date:depth))   #exclude data from a point to a point
+ 
+ try5 <- SACTN %>% 
+   mutate(kelvin = temp + 273.15)  #fix error in document 
+ 
+ #summarise #we can calc. mean, sd, min n max
+ SACTN %>% 
+   summarise(mean_temp = mean(temp, na.rm = TRUE),  #the names e,g sd_temp are the names that we name our data
+             sd_temp = sd(temp, na.rm = TRUE),    #can substitute true with T
+             min_temp = min(temp, na.rm = TRUE),
+             max_temp = max(temp, na.rm = TRUE)
+   )
+        
---
+++ Day4_Tidiest.R
@@ -0,0 +1,52 @@
+#Day 4
+#Chapter 12
+#Tidy data 3.0
+#1Feb 2019
+
+#load libraries
+library(tidyverse)
+library(lubridate)
+
+
+Load the data from a .RData file
+load("data/SACTNmonthly_v4.0.RData")
+
+# Copy the data as a dataframe with a shorter name
+SACTN <- SACTNmonthly_v4.0
+
+# Remove the original
+rm(SACTNmonthly_v4.0)
+
+
+# Group by depth
+
+# Calculate mean temperature by depth
+SACTN_depth_mean <- SACTN_depth %>% 
+  group_by(depth) %>%                              #group by depth
+  summarise(mean_temp = mean(temp, na.rm = TRUE), #using summarise to calc. mean 
+            count = n())
+SACTN_depth_mean
+
+
+ggplot(data = SACTN_depth_mean, mapping = aes(x = depth, y = mean_temp)) +
+  geom_point(aes(size = count), alpha = 1/3) +
+  geom_smooth(se = FALSE) +
+  ggtitle("A") +                                  #adding title
+  labs(x = "Depth", y = "Mean Temp")              #adding labels
+ 
+#selecting data greater than 30 years 
+SACTN_30_years <- SACTN %>%
+  group_by(site, src) %>%                        #weach site and source viewed individually
+  filter(n() > 360)
+
+# First create a character vector containing the desired sites
+selected_sites <- c("Paternoster", "Oudekraal", "Muizenberg", "Humewood")  #filtering more than 1 site #c (concatinate) creates a set
+
+# Then calculate the statistics
+SACTN %>% 
+  filter(site %in% selected_sites) %>%                #filter in site column in the selected sites folder
+  group_by(site, src) %>%                             #group the selected set of sites by site and src
+  summarise(mean_temp = mean(temp, na.rm = TRUE),     #sunnarise needs us to give a name for the mean/sd data
+            sd_temp = sd(temp, na.rm = TRUE))
+
+
